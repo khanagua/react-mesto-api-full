33f54193a33b8ac9b7937ba2206fb99a596d348c
Viktoria Khanagua <v.khanagua@yandex.ru>
2021-08-22 11:41:39 +0300

    правки cors

--- backend/app.js
+++ backend/app.js
@@ -61,6 +61,7 @@ app.use((req, res, next) => {
   if (reqMethod === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.end();
   }
 
   next();
