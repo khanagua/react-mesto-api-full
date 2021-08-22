1309e45757067e7af1c021ea33d1de660d54574d
Viktoria Khanagua <v.khanagua@yandex.ru>
2021-08-22 10:16:05 +0300

    правит эндепоинт auth

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
 // export const address = 'https://mesto.nomoreparties.co';
-export const address = 'https://api.mesto.khanagua.nomoredomains.rocks';
+export const BASE_URL = 'https://api.mesto.khanagua.nomoredomains.rocks';
 // export const token = 'b271f2bf-bb7f-4a18-b332-e480b04553ba';
 // export const cohort = 'cohort-24';
