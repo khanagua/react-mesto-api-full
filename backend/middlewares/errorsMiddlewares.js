b30aaa600f71816c6a79d09c8dfa6d8815843406
Viktoria Khanagua <v.khanagua@yandex.ru>
2021-08-28 22:32:23 +0300

    правит обработку неизвестного маршрута

--- backend/middlewares/errorsMiddlewares.js
+++ backend/middlewares/errorsMiddlewares.js
@@ -1,7 +1,7 @@
-module.exports.errorsMiddlewares = (err, req, res) => {
+module.exports.errorsMiddlewares = (err, req, res, next) => {
   const { statusCode = 500, message = 'Ошибка на сервере' } = err;
   res
     .status(statusCode)
     .send({ message });
-  // next();
+  next();
 };
